3bbac88f831542e7b0dc662c126160ae168fb87c
kev7862 <kevmatite7862@gmail.com>
2017-09-06 17:03:27 +0300

    Created an endangered_sighting route and a confirmation page template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -65,5 +65,20 @@ post("/animal/new", (request, response) -> {
   response.redirect("/");
     return null;
   });
+
+  post("/endangered_sighting", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  String rangerName = request.queryParams("rangerName");
+  int animalIdSelected = Integer.parseInt(request.queryParams("endangeredAnimalSelected"));
+  String latLong = request.queryParams("latLong");
+  Sighting sighting = new Sighting(animalIdSelected, latLong, rangerName);
+  sighting.save();
+  model.put("sighting", sighting);
+  model.put("animals", EndangeredAnimal.all());
+  String animal = EndangeredAnimal.find(animalIdSelected).getName();
+  model.put("animal", animal);
+  model.put("template", "templates/success.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
 }
 }
