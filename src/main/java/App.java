de355d39511dbe3849cb29033624db5ea70c605c
kev7862 <kevmatite7862@gmail.com>
2017-09-06 15:53:37 +0300

    changed my DB password

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,3 +19,21 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+
+    post("/sighting", (request, response) -> {
+  Map<String, Object> model = new HashMap<String, Object>();
+  String rangerName = request.queryParams("rangerName");
+  int animalIdSelected = Integer.parseInt(request.queryParams("animalSelected"));
+  String latLong = request.queryParams("latLong");
+  Sighting sighting = new Sighting(animalIdSelected, latLong, rangerName);
+  sighting.save();
+  model.put("sighting", sighting);
+  model.put("animals", Animal.all());
+  String animal = Animal.find(animalIdSelected).getName();
+  model.put("animal", animal);
+  model.put("template", "templates/success.vtl");
+  return new ModelAndView(model, layout);
+}, new VelocityTemplateEngine());
+}
+}
