8bf0f9efe486908d28137083b4b1553db6d07b6e
kev7862 <kevmatite7862@gmail.com>
2017-09-06 18:13:14 +0300

    // Additional logic for heroku deployment.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,6 +8,17 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+
+    // Additional logic for heroku deployment.
+    ProcessBuilder process = new ProcessBuilder();
+     Integer port;
+     if (process.environment().get("PORT") != null) {
+         port = Integer.parseInt(process.environment().get("PORT"));
+     } else {
+         port = 4567;
+     }
+
+    setPort(port);
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
